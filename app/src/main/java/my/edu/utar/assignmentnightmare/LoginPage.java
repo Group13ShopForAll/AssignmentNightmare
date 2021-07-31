298ca7dae87e9739c344818c83aba32ca5e78939
Shop4All <wongjunlin@gmail.com>
2021-07-31 16:49:04 +0800

    Firebase Connection + User login validation

--- app/src/main/java/my/edu/utar/assignmentnightmare/LoginPage.java
+++ app/src/main/java/my/edu/utar/assignmentnightmare/LoginPage.java
@@ -1,32 +1,104 @@
 package my.edu.utar.assignmentnightmare;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.view.WindowManager;
+import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginPage extends AppCompatActivity {
 
-    TextView butt;
+    //Done by Jiun Lin
+    private TextView btnLoginAccount, btnRegisterAccount, tvErrorLoginBanner;
+    private EditText edtLoginAccount, edtLoginPassword;
+    private ProgressBar pbLoginAcc;
+
+    private String loginAccount, loginPassword;
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_login_page);
-        butt = (TextView) findViewById(R.id.submitbutton);
 
-        butt.setOnClickListener(new View.OnClickListener() {
+        edtLoginAccount = (EditText) findViewById(R.id.edtLoginAccount);
+        edtLoginPassword = (EditText) findViewById(R.id.edtLoginPassword);
+        btnLoginAccount = (TextView) findViewById(R.id.btnLoginAccount);
+        btnRegisterAccount = (TextView) findViewById(R.id.btnRegisterAccount);
+        tvErrorLoginBanner = (TextView) findViewById(R.id.tvErrorLoginBanner);
+        pbLoginAcc = (ProgressBar) findViewById(R.id.pbLoginAcc);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        // when click on the register account button redirect to account registration page
+        btnRegisterAccount.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent( LoginPage.this, Homepagee.class);
-                startActivity(intent);
-                finish();
+                sendToRegisterAccPage();
             }
         });
 
+        // when click on the login button, perform input validation checking
+        btnLoginAccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                validateLoginInfo();
+            }
+        });
+
+    }
+
+    private void validateLoginInfo() {
+        loginAccount = edtLoginAccount.getText().toString();
+        loginPassword = edtLoginPassword.getText().toString();
+
+        if (TextUtils.isEmpty(loginAccount)){
+            edtLoginAccount.setError("Please type in your email account");
+            return;
+        }
+        if (TextUtils.isEmpty(loginPassword)){
+            edtLoginPassword.setError("Please type in your account password");
+            return;
+        }
+
+        pbLoginAcc.setVisibility(View.VISIBLE);
+        mAuth.signInWithEmailAndPassword(loginAccount,loginPassword).addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+            @Override
+            public void onSuccess(AuthResult authResult) {
+                pbLoginAcc.setVisibility(View.INVISIBLE);
+                tvErrorLoginBanner.setVisibility(View.INVISIBLE);
+                sendToHomePage();
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                pbLoginAcc.setVisibility(View.INVISIBLE);
+                tvErrorLoginBanner.setVisibility(View.VISIBLE);
+            }
+        });
+    }
+
+    private void sendToHomePage() {
+        startActivity(new Intent(LoginPage.this, Homepagee.class));
+    }
+
+    private void sendToRegisterAccPage() {
+        startActivity(new Intent(LoginPage.this, RegisterAccountPage.class));
     }
 }
---
+++ app/src/main/java/my/edu/utar/assignmentnightmare/RegisterAccountPage.java
@@ -0,0 +1,98 @@
+package my.edu.utar.assignmentnightmare;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class RegisterAccountPage extends AppCompatActivity {
+
+    private EditText edtRegAcc, edtRegPassword, edtConfirmPassword;
+    private TextView btnRegisterAcc;
+    private ProgressBar pbRegAcc;
+
+    private String emailAcc, password, confirmPassword;
+
+    private FirebaseAuth mAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register_account_page);
+
+        edtRegAcc = (EditText) findViewById(R.id.edtRegAccount);
+        edtRegPassword = (EditText) findViewById(R.id.edtPassword);
+        edtConfirmPassword = (EditText) findViewById(R.id.edtConfirmPassword);
+
+        pbRegAcc = (ProgressBar) findViewById(R.id.pbRegAcc);
+        btnRegisterAcc = (TextView) findViewById(R.id.btnRegAcc);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        // when user click on the register account button, functions trigger
+        btnRegisterAcc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //first check if the user had filled in all the required fields.
+                validateRegisterAccountInfo();
+            }
+        });
+
+    }
+
+    private void validateRegisterAccountInfo() {
+        emailAcc = edtRegAcc.getText().toString();
+        password = edtRegPassword.getText().toString();
+        confirmPassword = edtConfirmPassword.getText().toString();
+
+        if (TextUtils.isEmpty(emailAcc)){
+            edtRegAcc.setError("Please fill in email");
+            return;
+        }
+        if (TextUtils.isEmpty(password)){
+            edtRegPassword.setError("Please type your password");
+            return;
+        }
+        if (TextUtils.isEmpty(confirmPassword)){
+            edtConfirmPassword.setError("Please type your password again");
+            return;
+        }
+        if (!password.equals(confirmPassword)){
+            edtConfirmPassword.setError("Confirm password needs to be same as previous password");
+        }
+        // if the user had typed in all the required info, proceed to account registration action
+        pbRegAcc.setVisibility(View.VISIBLE);
+        registerAccount();
+    }
+
+    private void registerAccount() {
+        mAuth.createUserWithEmailAndPassword(emailAcc,password).addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+            @Override
+            public void onSuccess(AuthResult authResult) {
+                Toast.makeText(RegisterAccountPage.this, "Successfully created new account", Toast.LENGTH_SHORT).show();
+                sendToLoginPage();
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(RegisterAccountPage.this, "Failed created account, "+e.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private void sendToLoginPage() {
+        startActivity(new Intent(RegisterAccountPage.this, LoginPage.class));
+    }
+}
